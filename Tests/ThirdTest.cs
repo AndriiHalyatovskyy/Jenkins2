d3ffee10a818e8ec455e6e370f13d998ea3ef7db
AndriiHaliatovskyi <Andry1a294@gmail.com>
2020-04-10 13:37:07 +0300

    ome more test

---
+++ Tests/ThirdTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Jenkins2.Tests
+{
+	public class ThirdTest : BaseTest
+	{
+		[Test]
+		public void TestMethod()
+		{
+			Pages.Goole.DoSearch("wikipedia");
+			Pages.SearchResult.OpenSiteByTitle("wikipedia");
+			Assert.IsTrue(Pages.Wikipedia.getPageTitle().ToLower().Contains("Wikipedia".ToLower()));
+			Assert.True(Pages.Wikipedia.getPageTitle().ToLower().Contains("wiki"));
+		}
+	}
+}
